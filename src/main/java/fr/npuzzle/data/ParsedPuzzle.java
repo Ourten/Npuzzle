6e5ce82d96476dfe95d8c951eac96ccbf5c8d036
Ourten <ourtencoop@gmail.com>
2018-08-22 14:05:48 +0200

    Add ParsedPuzzle pojo

---
+++ src/main/java/fr/npuzzle/data/ParsedPuzzle.java
@@ -0,0 +1,110 @@
+package fr.npuzzle.data;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+public class ParsedPuzzle
+{
+    public static final int EMPTY = 0;
+
+    private int     size;
+    private int[][] grid;
+
+    public ParsedPuzzle(int size)
+    {
+        this.size = size;
+
+        this.grid = new int[size][];
+
+        for (int i = 0; i < this.grid.length; i++)
+        {
+            this.grid[i] = new int[size];
+        }
+    }
+
+    public int getSize()
+    {
+        return size;
+    }
+
+    public int[][] getGrid()
+    {
+        return grid;
+    }
+
+    public void setCell(int x, int y, int value)
+    {
+        if (value < 1)
+            throw new RuntimeException("Cell cannot be assigned to a number less than 1.");
+        this.grid[x][y] = value;
+    }
+
+    public void setEmpty(int x, int y)
+    {
+        this.grid[x][y] = 0;
+    }
+
+    public int getCell(int x, int y)
+    {
+        return this.grid[x][y];
+    }
+
+    public boolean isEmpty(int x, int y)
+    {
+        return this.getCell(x, y) == EMPTY;
+    }
+
+    public int[] getRow(int x)
+    {
+        return this.grid[x];
+    }
+
+    public int[] getColumn(int y)
+    {
+        int[] column = new int[this.size];
+
+        for (int i = 0; i < column.length; i++)
+            column[i] = this.grid[i][0];
+        return column;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append(this.size).append("x").append(this.size);
+
+        builder.append("\n");
+        for (int[] row : this.grid)
+        {
+            int index = 0;
+            for (int cell : row)
+            {
+                if (index != 0)
+                    builder.append(" ");
+                builder.append(cell);
+                index++;
+            }
+            builder.append("\n");
+        }
+        return builder.toString();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParsedPuzzle that = (ParsedPuzzle) o;
+        return getSize() == that.getSize() &&
+                Arrays.equals(getGrid(), that.getGrid());
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int result = Objects.hash(getSize());
+        result = 31 * result + Arrays.hashCode(getGrid());
+        return result;
+    }
+}
